ab0bf2f04c6ac1d6f384e8cdba991b703648eb76
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-13 21:59:39 +0400

    add loading spinner before to backend data

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -7,7 +7,8 @@
           >Load Submitted Experiences</base-button
         >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-if="!isLoading">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -29,10 +30,12 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = false;
       fetch(
         'https://vue-http-demo-d8f97-default-rtdb.europe-west1.firebasedatabase.app/surveys.json'
       )
@@ -42,6 +45,7 @@ export default {
           }
         })
         .then((data) => {
+          this.isLoading = false;
           const results = [];
           for (const id in data) {
             results.push({
@@ -54,9 +58,9 @@ export default {
         });
     },
   },
-  mounted(){
-    this.loadExperiences()
-  }
+  mounted() {
+    this.loadExperiences();
+  },
 };
 </script>
 
